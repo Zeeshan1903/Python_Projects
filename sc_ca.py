4b81b4488890c6515b192b8ce5c151b52eb7048b
zeeshan <rangrejroshanara@gmail.com>
2024-03-09 18:22:53 +0530

    9 March

--- sc_ca.py
+++ sc_ca.py
@@ -1,13 +1,46 @@
 variable = input('Enter the input for which you want the differentiation: ')
 print('Use the bracket if you are using the power function (power fn)')
 print("For power use ---> ^") 
-  
+print('Decimal representation is not yet been implemented so try not to give the input in the decimal representation')
+print('For cube root write 0.3333 this will give you the best approximation')
 ##Defining the power function
-def power(a,b):
+
+def power_(a,b):
     value = 1
     while(b!=0):
-        value *= a
+        value*=a
         b-=1
+    return value
+
+def power(a,b):
+    if(b>=1):
+        value = 1
+        while(b!=0):
+            value *= a
+            b-=1
+    elif(b == 1):
+        return 1
+    else:
+        value = 0
+        inverse_power = int(1/b)
+        for i in range(int(a)//2+2):
+    
+            if(i*i>a):
+        
+                value = i-1
+                break 
+    
+        if(power(value,inverse_power) != a):    
+            for i in range(10):
+                if(power((value+(i/10)),inverse_power) > a):
+                    value = value+(i-1)/10    
+                    break
+        if(power(value,inverse_power) != a):
+            for i in range(10):
+                if(power((value+(i/100)),inverse_power)) > a:
+                    value = value+(i-1)/100    
+                    break
+        
     return value
 
 
@@ -103,17 +136,7 @@ def checking(value_list,sign_list):
 
     print(value_list)
     print(sign_list)                
-        ##traverse then for + and - 
-    # for i in range(len(sign_list)):
-    #     if (sign_list[i] == '+' ) :
-    #         value_list[i] = float(value_list[i])
-    #         value_list[i+1] = float(value_list[i+1])
-                
-    #         value_list[i] = (value_list[i]+value_list[i+1])
-    #         value_list.remove(value_list[i+1])
-    #         sign_list.remove(sign_list[i])
-    #         value_list.append(0)
-    #         sign_list.append(0)
+    
                 
     for i in range(len(sign_list)):
         if (sign_list[i] == '-' ) :
